c12bab5a173e52e0efae85295f851568a9582f66
Huawei Weng <hweng@ucsd.edu>
2019-01-29 10:45:27 -0800

    Remove test folder

--- test/system/dashboards_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class DashboardsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit dashboards_url
-  #
-  #   assert_selector "h1", text: "Dashboard"
-  # end
-end
