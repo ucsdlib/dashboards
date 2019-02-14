774fa8b8ef7581285d173879b9ef4c49937c3a51
Huawei Weng <hweng@ucsd.edu>
2019-02-14 11:46:06 -0800

    #12 Add shoulda-matchers and Configuration

--- spec/models/dashboard_spec.rb
+++ spec/models/dashboard_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard, type: :model do
+
+  it {should validate_presence_of(:rdd_attribute)}
+  it {should validate_presence_of(:rdd_value)}
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/dashboards/index.html.erb_spec.rb
+++ spec/views/dashboards/index.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "dashboards/index", type: :view do
   before(:each) do
     assign(:dashboards, [
-      Dashboard.create!(),
-      Dashboard.create!()
+      Dashboard.create(rdd_attribute: "collections", rdd_value: 1),
+      Dashboard.create(rdd_attribute: "files", rdd_value: 6)
     ])
   end
 
